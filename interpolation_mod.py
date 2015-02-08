a94b0c33edf004d776cb3daa35a68eee0ae15add
mccombsp-kingco <mccombs@imperium.org>
2015-02-07 19:12:56 -0800

    switched cartesian interp from trig to geometry

--- interpolation_mod.py
+++ interpolation_mod.py
@@ -27,29 +27,25 @@ before_time_point_interval = time_point - before
 before_ratio = ((float(before_time_point_interval.days)*86400+
                       before_time_point_interval.seconds)/
                      (locs_time_interval.days*86400+locs_time_interval.seconds))
-cartesian_dist = math.sqrt(
-                 ((lats[order_pos]-lats[order_pos-1])**2)+
-                 ((lons[order_pos]-lons[order_pos-1])**2))
+lat_dist = (lats[order_pos]-lats[order_pos-1])
+lon_dist = (lons[order_pos]-lons[order_pos-1])
+interp_lat = lats[order_pos-1] + (lat_dist*before_ratio)
+interp_lon = lons[order_pos-1] + (lon_dist*before_ratio)
+interp_loc = (interp_lat,interp_lon)
  
-cart_slope = (float(lats[order_pos]-lats[order_pos-1])/
-                   (lons[order_pos]-lons[order_pos-1]))
-
-cart_konstant = ((cartesian_dist * before_ratio) / math.sqrt(1+(cart_slope**2)))*-1
-
-interp_loc = ((lats[order_pos-1]+cart_konstant),
-             (lons[order_pos-1]+(cart_slope*cart_konstant)))
-
-check_slope = (float(interp_loc[0]-lats[order_pos-1])/
-                    (interp_loc[1]-lons[order_pos-1]))
+# cart_slope = (float(lats[order_pos]-lats[order_pos-1])/
+#                    (lons[order_pos]-lons[order_pos-1]))
+# cart_konstant = ((cartesian_dist * before_ratio) / math.sqrt(1+(cart_slope**2)))*-1
+# interp_loc = ((lats[order_pos-1]+cart_konstant),
+#              (lons[order_pos-1]+(cart_slope*cart_konstant)))
+# check_slope = (float(interp_loc[0]-lats[order_pos-1])/
+#                     (interp_loc[1]-lons[order_pos-1]))
 
 print locs[order_pos - 1], before
 print locs[order_pos], after
 print "Time between two locations:", locs_time_interval
 print "Time between before location and time point:", before_time_point_interval
 print "Ratio of before - time point to before - after:", before_ratio
-print "Cartesian distance in 'degrees':", cartesian_dist
-print "Cartesian interpolated distance in 'degrees':", (cartesian_dist * before_ratio)
-print "Cartesian slope is:", cart_slope
-print "Cartesian Konstant is:", cart_konstant
+print "Latitude distance in 'degrees':", lat_dist
+print "Longitude distance in 'degrees':", lon_dist
 print "New location is: ", interp_loc
-print "Check slope is: ", check_slope 
